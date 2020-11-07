7f002f947aedf18c1285bc30964d0d25b1398024
Rodrigo Illera <illera@strands.com>
2020-11-07 14:42:56 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. This has been edited by Rodrigo Illera. Now in English.
         </p>
         <a
           className="App-link"
--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders author', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/illera/i);
+  expect(linkElement).toBeInTheDocument();
+});
